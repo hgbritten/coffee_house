9e0151679609710cba85921bb106a2a567dec35f
hunter britten <huntergbritten@gmail.com>
2021-02-24 18:43:50 -0800

    add3 changed

---
+++ app2.js
@@ -0,0 +1,5 @@
+if (userName == 'Hunter'){
+    document.write('<h3>' + 'Welcome Lord Britten, King of the Gods' + '<h3>');
+} else {
+    document.write('<h3>' + 'Welcome ' + userName + '<h3>');
+}
---
+++ app3.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+}
+
+document.write('<h3>' + greeting + '<h3>');
